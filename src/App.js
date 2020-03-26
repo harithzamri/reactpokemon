4325b98921e3cfc5312fa3753c5928ce1c4dcb06
harithzamri <harithtoikee96@gmail.com>
2020-03-26 16:34:03 +0800

    learn about router

--- src/App.js
+++ src/App.js
@@ -5,15 +5,26 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import Navbar from "./Component/Layout/Navbar";
 import Dashboard from "./Component/Layout/Dashboard";
+import { HashRouter as Router, Route, Switch } from "react-router-dom";
+import PokemonDetails from "./Component/Pokemon/PokemonDetails";
 
 function App() {
   return (
-    <div className="App">
-      <Navbar />
-      <div className="container">
-        <Dashboard />
+    <Router>
+      <div className="App">
+        <Navbar />
+        <div className="container">
+          <Switch>
+            <Route exact path="/" component={Dashboard} />
+            <Route
+              exatch
+              path="/pokemon/:pokemonIndex"
+              component={PokemonDetails}
+            />
+          </Switch>
+        </div>
       </div>
-    </div>
+    </Router>
   );
 }
 
--- src/Component/Pokemon/PokemonCard.js
+++ src/Component/Pokemon/PokemonCard.js
@@ -3,10 +3,37 @@
 import React, { Component } from "react";
 import styled from "styled-components";
 import spinner from "../Pokemon/spinner.gif";
+import { Link } from "react-router-dom";
 
 const Sprite = styled.img`
   width: 5em;
   height: 5em;
+`;
+
+const Card = styled.div`
+  opacity: 0.95;
+  box-shadow: 0 1px 3px rgba(0, 0, 0, 0.12), 0 1px 2px rgba(0, 0, 0, 0.24);
+  transition: all 0.3s cubic-bezier(0.25, 0.8, 0.25, 1);
+  &:hover {
+    box-shadow: 0 14px 28px rgba(0, 0, 0, 0.25), 0 10px 10px rgba(0, 0, 0, 0.22);
+  }
+  -moz-user-select: none;
+  -website-user-select: none;
+  -ms-user-select: none;
+  user-select: none;
+  -o-user-select: none;
+`;
+
+const StyledLink = styled(Link)`
+  text-decoration: none;
+  color: black;
+  &:focus,
+  &:hover,
+  &:visited,
+  &:link,
+  &:active {
+    text-decoration: none;
+  }
 `;
 
 export default class PokemonCard extends Component {
@@ -33,45 +60,50 @@ export default class PokemonCard extends Component {
   render() {
     return (
       <div className="col-md-3 col-sm-6 mb-5">
-        <div className="card">
-          <h5 className="card-header">{this.state.pokemonIndex}</h5>
-          {this.state.imageLoading ? (
-            <img
-              src={spinner}
-              style={{ width: "5em", height: "5em" }}
-              className="card-img-top rounded mx-auto d-block mt-2"
+        <StyledLink to={`pokemon/${this.state.pokemonIndex}`}>
+          <Card className="card">
+            <h5 className="card-header">{this.state.pokemonIndex}</h5>
+            {this.state.imageLoading ? (
+              <img
+                src={spinner}
+                style={{ width: "5em", height: "5em" }}
+                className="card-img-top rounded mx-auto d-block mt-2"
+              />
+            ) : null}
+            <Sprite
+              className="card-img-top rounded mx-auto mt-2"
+              onLoad={() => this.setState({ imageLoading: false })}
+              onError={() => this.setState({ toManyRequest: true })}
+              src={this.state.imageUrl}
+              style={
+                this.state.toManyRequest
+                  ? { display: "none" }
+                  : this.state.imageLoading
+                  ? null
+                  : { display: "block" }
+              }
             />
-          ) : null}
-          <Sprite
-            className="card-img-top rounded mx-auto mt-2"
-            onLoad={() => this.setState({ imageLoading: false })}
-            onError={() => this.setState({ toManyRequest: true })}
-            src={this.state.imageUrl}
-            style={
-              this.state.toManyRequest
-                ? { display: "none" }
-                : this.state.imageLoading
-                ? null
-                : { display: "block" }
-            }
-          />
-          {this.state.toManyRequest ? (
-            <h6 className="mx-auto">
-              <span className="badge badge-danger mt-2">Too Many Request</span>
-            </h6>
-          ) : null}
-          <div className="card-body mx-auto">
-            <h6 className="card-title">
-              {this.state.name
-                .toLowerCase()
-                .split(" ")
-                .map(
-                  letter => letter.charAt(0).toUpperCase() + letter.substring(1)
-                )
-                .join(" ")}
-            </h6>
-          </div>
-        </div>
+            {this.state.toManyRequest ? (
+              <h6 className="mx-auto">
+                <span className="badge badge-danger mt-2">
+                  Too Many Request
+                </span>
+              </h6>
+            ) : null}
+            <div className="card-body mx-auto">
+              <h6 className="card-title">
+                {this.state.name
+                  .toLowerCase()
+                  .split(" ")
+                  .map(
+                    letter =>
+                      letter.charAt(0).toUpperCase() + letter.substring(1)
+                  )
+                  .join(" ")}
+              </h6>
+            </div>
+          </Card>
+        </StyledLink>
       </div>
     );
   }
